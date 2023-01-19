f7c13f864d7ddc9a5b8f1396b480b6781fa26660
liliw-w <liliw1@uchicago.edu>
2023-01-19 17:24:11 -0600

    change year to 2023

--- configuration.R
+++ configuration.R
@@ -1,14 +1,14 @@
 campaigns = list(
-  data.frame(campaign = 'Fall 2021', start = '2021-09-12', stop = '2022-01-01',
+  data.frame(campaign = 'Fall 2024', start = '2023-09-12', stop = '2024-01-01',
              stringsAsFactors = FALSE),
-  data.frame(campaign = 'Winter 2022', start = '2022-01-02', stop = '2022-03-24',
+  data.frame(campaign = 'Winter 2023', start = '2023-01-02', stop = '2023-03-24',
              stringsAsFactors = FALSE),
-  data.frame(campaign = 'Spring 2022', start = '2022-03-25', stop = '2022-06-12',
+  data.frame(campaign = 'Spring 2023', start = '2023-03-25', stop = '2023-06-12',
              stringsAsFactors = FALSE),
-  data.frame(campaign = 'Summer 2022', start = '2022-06-13', stop = '2022-09-11',
+  data.frame(campaign = 'Summer 2023', start = '2023-06-13', stop = '2023-09-11',
              stringsAsFactors = FALSE)
   )
 campaigns = dplyr::bind_rows(campaigns)
 campaigns = dplyr::mutate(campaigns, start = as.Date(start), stop = as.Date(stop))
 
-default_current_campaign = dplyr::filter(campaigns, campaign == 'Fall 2021')
+default_current_campaign = dplyr::filter(campaigns, campaign == 'Winter 2023')
