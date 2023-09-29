ce3e97da9a5ef33ce13637b1926e7f2031157c26
liliw-w <liliw1@uchicago.edu>
2023-09-29 16:07:50 -0500

    change compaign date

--- configuration.R
+++ configuration.R
@@ -1,10 +1,21 @@
 campaigns = list(
-  data.frame(campaign = '2023', start = '2023-01-01', stop = '2024-01-01',
+  data.frame(campaign = '2023 Winter', start = '2022-12-01', stop = '2023-03-01',
              stringsAsFactors = FALSE),
-  data.frame(campaign = '2024', start = '2024-01-02', stop = '2025-01-01',
+  data.frame(campaign = '2023 Spring', start = '2023-03-02', stop = '2023-06-01',
+             stringsAsFactors = FALSE),
+  data.frame(campaign = '2023 Summer', start = '2023-06-02', stop = '2023-09-01',
+             stringsAsFactors = FALSE),
+  data.frame(campaign = '2023 Fall', start = '2023-09-02', stop = '2023-12-01',
+             stringsAsFactors = FALSE),
+  data.frame(campaign = '2024', start = '2024-12-02', stop = '2025-01-01',
              stringsAsFactors = FALSE)
   )
 campaigns = dplyr::bind_rows(campaigns)
 campaigns = dplyr::mutate(campaigns, start = as.Date(start), stop = as.Date(stop))
 
-default_current_campaign = dplyr::filter(campaigns, campaign == '2023')
+
+current_date = Sys.Date()
+default_current_campaign = dplyr::filter(
+  campaigns, 
+  start <= !!current_date & !!current_date <= stop
+)
