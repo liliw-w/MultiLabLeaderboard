5e03bec5dd2635aa2c878b84f68f1fc7c635c5f3
liliw-w <liliw1@uchicago.edu>
2023-12-05 15:35:57 -0600

    add more compaign dates

--- configuration.R
+++ configuration.R
@@ -1,5 +1,5 @@
 campaigns = list(
-  data.frame(campaign = '2023 Winter', start = '2022-12-01', stop = '2023-03-01',
+  data.frame(campaign = '2022 Winter', start = '2022-12-01', stop = '2023-03-01',
              stringsAsFactors = FALSE),
   data.frame(campaign = '2023 Spring', start = '2023-03-02', stop = '2023-06-01',
              stringsAsFactors = FALSE),
@@ -7,7 +7,11 @@ campaigns = list(
              stringsAsFactors = FALSE),
   data.frame(campaign = '2023 Fall', start = '2023-09-02', stop = '2023-12-01',
              stringsAsFactors = FALSE),
-  data.frame(campaign = '2024', start = '2024-12-02', stop = '2025-01-01',
+  data.frame(campaign = '2024 Winter', start = '2023-12-02', stop = '2024-03-01',
+             stringsAsFactors = FALSE),
+  data.frame(campaign = '2024 Spring', start = '2024-03-02', stop = '2024-06-01',
+             stringsAsFactors = FALSE),
+  data.frame(campaign = '2024 Summer', start = '2024-06-02', stop = '2024-09-01',
              stringsAsFactors = FALSE)
   )
 campaigns = dplyr::bind_rows(campaigns)
@@ -16,6 +20,6 @@ campaigns = dplyr::mutate(campaigns, start = as.Date(start), stop = as.Date(stop
 
 current_date = Sys.Date()
 default_current_campaign = dplyr::filter(
-  campaigns, 
+  campaigns,
   start <= !!current_date & !!current_date <= stop
 )
